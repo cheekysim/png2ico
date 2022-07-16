b31043a7ddbd2f0f2136a86b22821247cc3c2f1b
cheekysim <minecraftcheekysim@gmail.com>
2022-07-16 23:50:45 +0100

    remove print lines

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from PIL import Image
 import os
 
-print(os.listdir(os.getcwd()))
 psizes = ["16", "24", "32", "48", "64", "96", "128", "256"]
 sizes, images = [], []
 [images.append(Image.open(str(file))) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
 [sizes.append((int(file.lower().split(".")[0]),int(file.lower().split(".")[0]))) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
-print(sizes)
-print((str(sizes[-1:]).split(",")[0])[2:])
 img = Image.open((str(sizes[-1:]).split(",")[0])[2:] + ".png")
 img.save('icon.ico', sizes=sizes, append_images=images)
