d269ef22616fd1e7abe540614e24f33c6c92bd1b
cheekysim <minecraftcheekysim@gmail.com>
2022-07-16 22:32:36 +0100

    updated ico

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ psizes = ["16", "24", "32", "48", "64", "96", "128", "255"]
 sizes, images = [], []
 [images.append(file) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
 [sizes.append((int(size.split('.')[0]),int(size.split('.')[0]))) for size in images]
-print(sizes)
 img = Image.open(str(images[-1:])[2:-2])
 img.save('icon.ico', sizes=sizes)
