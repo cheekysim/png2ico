2149794dacaae331c00399b6e68e6f3c98302080
cheekysim <minecraftcheekysim@gmail.com>
2022-07-18 08:54:43 +0100

    fixed program to includ all sizes

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from PIL import Image
 import os
 
 psizes = ["16", "24", "32", "48", "64", "96", "128", "256"]
-sizes, images, msizes, tsizes = [], [], [], []
-[images.append(Image.open(str(file))) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
-[sizes.append((int(file.lower().split(".")[0]),int(file.lower().split(".")[0]))) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
-[tsizes.append(str(file.split(".")[0])) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
-[msizes.append(size) for size in psizes if size not in tsizes]
+images = [Image.open(str(file)) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
+sizes = [int(file.lower().split(".")[0]) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
+sizes.sort()
+sizes = [(size, size) for size in sizes]
+tsizes = [str(file.split(".")[0]) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
+msizes = [size for size in psizes if size not in tsizes]
 convert = "n"
 if msizes:
     convert = input("You are missing the following size(s):\n" + ", ".join(msizes) + "\nWould you like to add them | Y / N\n")
@@ -15,12 +16,12 @@ if convert.lower() == "y":
     for size in msizes:
         img = Image.open((str(sizes[-1:]).split(",")[0])[2:] + ".png")
         tsize = (int(size), int(size))
-        img = img.resize(tsize, resample=2) 
+        img = img.resize((tsize), resample=2) 
         img.save(f'{size}.png')
-    images = []
-    [images.append(Image.open(str(file))) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
+    images = [Image.open(str(file)) for file in os.listdir(os.getcwd()) if file.lower().split(".")[0] in psizes]
     img = Image.open("256.png")
     img.save('icon.ico', sizes=[(16, 16), (24, 24), (32, 32), (48, 48), (64, 64), (96, 96), (128, 128), (256, 256)], append_images=images)
 else:
     img = Image.open((str(sizes[-1:]).split(",")[0])[2:] + ".png")
+    print((str(sizes[-1:]).split(",")[0])[2:] + ".png")
     img.save('icon.ico', sizes=sizes, append_images=images)
